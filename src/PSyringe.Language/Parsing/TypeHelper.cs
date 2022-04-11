888ff1cca864fe1cb676dc61cb1cb177252b0026
Michael <44098020+M1CH3L1US@users.noreply.github.com>
2022-04-11 16:19:02 -0700

    🗑️ [lang] Implemented as part of Language.TypeHelper

--- src/PSyringe.Language/Parsing/TypeHelper.cs
+++
@@ -1,9 +0,0 @@
-using System.Management.Automation.Language;
-
-namespace PSyringe.Language.Parsing;
-
-public class TypeHelper {
-  public static bool GetInjectAttribute(IReadOnlyCollection<AttributeBaseAst> asts) {
-    return false;
-  }
-}
