85deca77e7ce7f76198f78fd9bc0993816caa7c6
Michael <michi.straub@icloud.com>
2022-04-24 06:39:44 +0200

    ✏️ [tool] Test benchmarking with example file

--- src/PSyringe.Tool/Program.cs
+++ src/PSyringe.Tool/Program.cs
@@ -1,18 +1,33 @@
-﻿using System.Management.Automation;
+﻿using System.Management.Automation.Language;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 using PSyringe.Language.AstTransformation;
-using PSyringe.Language.Compiler;
 using PSyringe.Language.Parsing;
 
 namespace PSyringe.Tool;
 
+/// <summary>
+///   |          Method |    Mean |    Error |   StdDev |      Gen 0 |     Gen 1 | Allocated |
+///   |---------------- |--------:|---------:|---------:|-----------:|----------:|----------:|
+///   | ToStringFromAst | 1.222 s | 0.0145 s | 0.0121 s | 39000.0000 | 1000.0000 |    318 MB |
+/// </summary>
 public static class Program {
   public static void Main(string[] args) {
-    var filepath = "C:/Temp/master.ps1";
-    var parser = new ScriptParser(new ElementFactory());
-    var file = File.ReadAllText(filepath);
-    
-    var script = parser.Parse(file);
-    var text = script.ScriptBlock.ToStringFromAst();
-    Console.WriteLine(text);
+    BenchmarkRunner.Run<Benchy>();
+  }
+
+  [MemoryDiagnoser]
+  public class Benchy {
+    public static ScriptExtent Extent = new(null, null);
+
+    public static string Code = File.ReadAllText("C:/Temp/master.ps1");
+
+    [Benchmark]
+    public string ToStringFromAst() {
+      var parser = new ScriptParser(new ElementFactory());
+      var script = parser.Parse(Code);
+      var text = script.ScriptBlock.ToStringFromAst();
+      return text;
+    }
   }
 }
